b0bc3c409f9ef93e8e8fda485ac410781fb59412
Prashik-Sasane <prashiksasane85@gmail.com>
2025-11-04 22:31:10 +0530

    feat: added auth and landing page

--- backend/app.js
+++ backend/app.js
@@ -15,8 +15,8 @@ app.use("/api/auth", authRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB Connected"))
-  .catch((err) => console.error("❌ MongoDB Error:", err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB Error:", err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
